12e5d35b01a760e2e53866f2fb7303a1ec9b33ac
roja <roja@signalx.ai>
2020-08-03 15:56:20 +0530

    updated static file path

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,6 +92,10 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),
 
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
 
     }
 
